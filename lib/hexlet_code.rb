53ded0c9ca7342906e66d6387937610f57bafa5f
masi <masi@fozzy.com>
2024-04-02 11:37:14 +0300

    rename form folder

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -5,10 +5,10 @@ require_relative 'hexlet_code/version'
 # Hexlet module for form generate
 module HexletCode
   autoload :Tag, 'hexlet_code/tag'
-  autoload :Form, 'hexlet_code/form_lib/form'
+  autoload :Form, 'hexlet_code/elements/form'
   autoload :FormBuilder, 'hexlet_code/form_builder'
   autoload :FormRenderer, 'hexlet_code/form_renderer'
-  autoload :Inputs, 'hexlet_code/form_lib/inputs/inputs'
+  autoload :Inputs, 'hexlet_code/elements/inputs/inputs'
   class Error < StandardError; end
 
   def self.form_for(entity, attributes = {})
---
+++ lib/hexlet_code/elements/inputs/inputs.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module HexletCode
+  # Inputs builder
+  module Inputs
+    autoload :TextInput, 'hexlet_code/elements/inputs/text_input'
+    autoload :BaseInput, 'hexlet_code/elements/inputs/base_input'
+    autoload :StringInput, 'hexlet_code/elements/inputs/string_input'
+  end
+end
