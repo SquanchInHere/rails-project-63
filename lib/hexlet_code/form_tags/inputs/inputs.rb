f563c14e415cd62c782791251069fa8b4e458129
masi <masi@fozzy.com>
2024-04-02 11:27:15 +0300

    remove tag folder

--- lib/hexlet_code/form_tags/inputs/inputs.rb
+++ lib/hexlet_code/form_tags/inputs/inputs.rb
@@ -3,8 +3,8 @@
 module HexletCode
   # Inputs builder
   module Inputs
-    autoload :TextInput, 'hexlet_code/form_tags/inputs/tags/text_input'
-    autoload :BaseInput, 'hexlet_code/form_tags/inputs/tags/base_input'
-    autoload :StringInput, 'hexlet_code/form_tags/inputs/tags/string_input'
+    autoload :TextInput, 'hexlet_code/inputs/tags/text_input'
+    autoload :BaseInput, 'hexlet_code/inputs/tags/base_input'
+    autoload :StringInput, 'hexlet_code/inputs/tags/string_input'
   end
 end
